29e68c92ed7c4f7d86ec49211f4e0f8027a45e0f
Ami Hays <amihays2@gmail.com>
2015-10-16 17:33:03 -0700

    Add some project show page styling

--- app/assets/javascripts/components/project_show.js.jsx
+++ app/assets/javascripts/components/project_show.js.jsx
@@ -30,19 +30,46 @@ window.ProjectShow = React.createClass({
     }
 
     return(
-      <div className='container'>
+      <div className='container project-show-container'>
         <div className='row'>
-          <div className='col-sm-8 col-sm-offset-2'>
-            <div className='project-header'>
+          <div className='col-sm-12'>
+            <div className='project-title'>
               <h1>{this.state.project.title}</h1>
             </div>
           </div>
         </div>
         <div className='row'>
-          <div className='col-sm-8 col-sm-offset-2'>
-            <div className='project-show-image-container'>
-              <img src={this.state.project.image_url} />
+          <div className='col-sm-6'>
+            <img className='project-show-image' src={this.state.project.image_url} />
+          </div>
+          <div className='col-sm-5 col-sm-offset-1'>
+            <div className='project-show-sidebar'>
+
             </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <h3>Artist</h3>
+          </div>
+          <div className='col-sm-8 project-artist'>
+            <h3 className='grey'>{this.state.project.artist_name}</h3>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <h3>Funding Goal</h3>
+          </div>
+          <div className='col-sm-8 project-funding-goal'>
+            <h3 className='grey'>{this.state.project.funding_goal}</h3>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <h3>Description</h3>
+          </div>
+          <div className='col-sm-8 project-description'>
+            <h3 className='grey'>{this.state.project.description}</h3>
           </div>
         </div>
       </div>
